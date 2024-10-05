a00a1afa043ffe0c3364a5c02b551442a8efbed3
Oxters <oxters168@gmail.com>
2024-10-04 22:05:38 -0400

    Got QR code generation working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.zxing)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/OxGames/Pluvia/MainActivity.kt
+++ app/src/main/java/com/OxGames/Pluvia/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.OxGames.Pluvia.ui.theme.PluviaTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +25,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    QrCodeImage(content = "https://google.com", size = 256.dp)
                 }
             }
         }
