aca5b91c114b70a33db342abb31dcbd960d10c3b
LossyDragon <thedragon1012@gmail.com>
2024-12-23 14:35:13 -0600

    Add NotificationHelper class.

---
+++ app/src/main/java/com/OxGames/Pluvia/NotificationHelper.kt
@@ -0,0 +1,76 @@
+package com.OxGames.Pluvia
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.net.toUri
+
+class NotificationHelper(private val context: Context) {
+    companion object {
+        private const val CHANNEL_ID = "pluvia_foreground_service"
+        private const val CHANNEL_NAME = "Pluvia Foreground Service"
+        private const val NOTIFICATION_ID = 1
+    }
+
+    private val notificationManager: NotificationManager =
+        context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+    init {
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_LOW
+        ).apply {
+            description = "Allows to display Pluvia foreground notifications"
+            setShowBadge(false)
+        }
+
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    fun notify(content: String) {
+        val notification = createForegroundNotification(content)
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    fun cancel() {
+        notificationManager.cancel(NOTIFICATION_ID)
+    }
+
+    fun createForegroundNotification(content: String): Notification {
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            "pluvia://home".toUri(),
+            context,
+            MainActivity::class.java
+        ).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        return NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle(context.getString(R.string.app_name))
+            .setContentText(content)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_MIN)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setContentIntent(pendingIntent)
+            .build()
+    }
+}
