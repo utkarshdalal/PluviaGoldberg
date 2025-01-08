dddefe31b4b21780235a9b613731e0c6e30be21d
Oxters <oxters168@gmail.com>
2025-01-08 15:05:31 -0500

    Fixed an issue where message dialogs would disappear on configuration change

--- app/src/main/java/com/OxGames/Pluvia/ui/component/dialog/state/MessageDialogState.kt
+++ app/src/main/java/com/OxGames/Pluvia/ui/component/dialog/state/MessageDialogState.kt
@@ -1,15 +1,43 @@
 package com.OxGames.Pluvia.ui.component.dialog.state
 
+import androidx.compose.runtime.saveable.mapSaver
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.OxGames.Pluvia.ui.enums.DialogType
 
 data class MessageDialogState(
     val visible: Boolean,
-    val onDismissRequest: (() -> Unit)? = null,
-    val onConfirmClick: (() -> Unit)? = null,
-    val onDismissClick: (() -> Unit)? = null,
+    val type: DialogType = DialogType.NONE,
     val confirmBtnText: String = "Confirm",
     val dismissBtnText: String = "Dismiss",
     val icon: ImageVector? = null,
     val title: String? = null,
     val message: String? = null,
-)
+) {
+    companion object {
+        val Saver = mapSaver(
+            save = { state ->
+                mapOf(
+                    "visible" to state.visible,
+                    "type" to state.type,
+                    "confirmBtnText" to state.confirmBtnText,
+                    "dismissBtnText" to state.dismissBtnText,
+                    // this will probably break once a dialog is used with an icon
+                    "icon" to state.icon,
+                    "title" to state.title,
+                    "message" to state.message,
+                )
+            },
+            restore = { savedMap ->
+                MessageDialogState(
+                    visible = savedMap["visible"] as Boolean,
+                    type = savedMap["type"] as DialogType,
+                    confirmBtnText = savedMap["confirmBtnText"] as String,
+                    dismissBtnText = savedMap["dismissBtnText"] as String,
+                    icon = savedMap["icon"] as ImageVector?,
+                    title = savedMap["title"] as String?,
+                    message = savedMap["message"] as String?,
+                )
+            }
+        )
+    }
+}
