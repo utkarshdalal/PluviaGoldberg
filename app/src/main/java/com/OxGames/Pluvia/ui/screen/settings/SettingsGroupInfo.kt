f2d301c747a95cbb16bb2a53771a695337236dcc
Oxters <oxters168@gmail.com>
2025-01-08 16:15:16 -0500

    Changed startup dialog message and added condition to show only if not tipped. Added button in settings to send tip. Added switch in settings to disable tip dialog on startup.

--- app/src/main/java/com/OxGames/Pluvia/ui/screen/settings/SettingsGroupInfo.kt
+++ app/src/main/java/com/OxGames/Pluvia/ui/screen/settings/SettingsGroupInfo.kt
@@ -1,19 +1,34 @@
 package com.OxGames.Pluvia.ui.screen.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MonetizationOn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
+import com.OxGames.Pluvia.PrefManager
 import com.alorma.compose.settings.ui.SettingsGroup
 import com.alorma.compose.settings.ui.SettingsMenuLink
+import com.alorma.compose.settings.ui.SettingsSwitch
 
 @Composable
 fun SettingsGroupInfo() {
     SettingsGroup(title = { Text(text = "Info") }) {
         val uriHandler = LocalUriHandler.current
+        var askForTip by remember { mutableStateOf(!PrefManager.tipped) }
 
         SettingsMenuLink(
             title = { Text(text = "Source code") },
@@ -45,6 +60,32 @@ fun SettingsGroupInfo() {
             onClick = {
                 /* Could link to pluvia repo to credits page? */
             },
+        )
+
+        SettingsMenuLink(
+            title = {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text("Send tip")
+                    Icon(imageVector = Icons.Filled.MonetizationOn, contentDescription = "Tip")
+                }
+            },
+            onClick = {
+                uriHandler.openUri("https://buy.stripe.com/5kAaFU1bx2RFeLmbII")
+                askForTip = false
+                PrefManager.tipped = !askForTip
+            },
+        )
+
+        SettingsSwitch(
+            state = askForTip,
+            title = { Text("Ask for tip on startup") },
+            onCheckedChange = {
+                askForTip = it
+                PrefManager.tipped = !askForTip
+            }
         )
 
         SettingsMenuLink(
