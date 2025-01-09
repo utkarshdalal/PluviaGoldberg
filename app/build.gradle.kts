acd922585a4d15e6b7caefd4852406e29ac62a9a
LossyDragon <thedragon1012@gmail.com>
2025-01-09 13:44:37 -0600

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         minSdk = 29
         targetSdk = 34
 
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         buildConfigField("boolean", "GOLD", "false")
         val iconValue = "@mipmap/ic_launcher"
