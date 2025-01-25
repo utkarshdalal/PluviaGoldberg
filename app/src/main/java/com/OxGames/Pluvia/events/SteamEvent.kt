6803c150aba990044767b98c6865c3a8cfc66192
LossyDragon <thedragon1012@gmail.com>
2025-01-24 23:26:30 -0600

    Handle error events when trying to sign in. When a login fail happens, the connection to steam will gracefully reconnect. Add event RemotelyDisconnected. Expose login errors to the UI. Show errors via snackbar.

--- app/src/main/java/com/OxGames/Pluvia/events/SteamEvent.kt
+++ app/src/main/java/com/OxGames/Pluvia/events/SteamEvent.kt
@@ -6,12 +6,13 @@ import com.OxGames.Pluvia.enums.LoginResult
 sealed interface SteamEvent<T> : Event<T> {
     data class Connected(val isAutoLoggingIn: Boolean) : SteamEvent<Unit>
     data class LoggedOut(val username: String?) : SteamEvent<Unit>
-    data class LogonEnded(val username: String?, val loginResult: LoginResult) : SteamEvent<Unit>
+    data class LogonEnded(val username: String?, val loginResult: LoginResult, val message: String? = null) : SteamEvent<Unit>
     data class LogonStarted(val username: String?) : SteamEvent<Unit>
     data class PersonaStateReceived(val persona: SteamFriend?) : SteamEvent<Unit>
-    data class QrAuthEnded(val success: Boolean) : SteamEvent<Unit>
+    data class QrAuthEnded(val success: Boolean, val message: String? = null) : SteamEvent<Unit>
     data class QrChallengeReceived(val challengeUrl: String) : SteamEvent<Unit>
     data object AppInfoReceived : SteamEvent<Unit>
     data object ForceCloseApp : SteamEvent<Unit>
     data object Disconnected : SteamEvent<Unit>
+    data object RemotelyDisconnected : SteamEvent<Unit>
 }
