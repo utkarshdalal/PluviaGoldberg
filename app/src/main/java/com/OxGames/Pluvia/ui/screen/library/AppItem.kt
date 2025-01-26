14109fdd20f992b11e9a78c2730eab96ac7ee238
LossyDragon <thedragon1012@gmail.com>
2025-01-25 22:34:22 -0600

    Make the library item transparent. Allows it to be color matched to the background its on top of.

--- app/src/main/java/com/OxGames/Pluvia/ui/screen/library/AppItem.kt
+++ app/src/main/java/com/OxGames/Pluvia/ui/screen/library/AppItem.kt
@@ -2,13 +2,17 @@ package com.OxGames.Pluvia.ui.screen.library
 
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.OxGames.Pluvia.data.AppInfo
 import com.OxGames.Pluvia.ui.internal.fakeAppInfo
@@ -23,6 +27,9 @@ fun AppItem(
 ) {
     ListItem(
         modifier = modifier.clickable { onClick() },
+        colors = ListItemDefaults.colors(
+            containerColor = Color.Transparent,
+        ),
         headlineContent = { Text(text = appInfo.name) },
         leadingContent = {
             ListItemImage { appInfo.clientIconUrl }
@@ -38,6 +45,7 @@ private fun Preview_AppItem() {
             LazyColumn {
                 items(List(5) { fakeAppInfo(it) }) {
                     AppItem(appInfo = it, onClick = {})
+                    HorizontalDivider(modifier = Modifier.fillMaxWidth())
                 }
             }
         }
