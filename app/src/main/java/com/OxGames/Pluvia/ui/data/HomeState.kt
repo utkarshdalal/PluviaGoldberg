8915d99a2fff81689ba41c6d72b73beb80cd72af
LossyDragon <thedragon1012@gmail.com>
2025-01-31 10:34:57 -0600

    Nit: Back handler should go back to  start screen set by preference.

--- app/src/main/java/com/OxGames/Pluvia/ui/data/HomeState.kt
+++ app/src/main/java/com/OxGames/Pluvia/ui/data/HomeState.kt
@@ -4,7 +4,6 @@ import com.OxGames.Pluvia.PrefManager
 import com.OxGames.Pluvia.ui.enums.HomeDestination
 
 data class HomeState(
-    // Allow user to set screen for launch. But we also respect pressing back to go to the Library.
     val currentDestination: HomeDestination = PrefManager.startScreen,
     val confirmExit: Boolean = false,
 )
