672e408b85aabbe7b2fac158ea9cad15123df5a2
Oxters <oxters168@gmail.com>
2025-02-13 11:43:48 -0500

    Bumped version and removed some todos

--- app/src/main/java/com/OxGames/Pluvia/ui/data/LibraryState.kt
+++ app/src/main/java/com/OxGames/Pluvia/ui/data/LibraryState.kt
@@ -5,11 +5,6 @@ import com.OxGames.Pluvia.data.LibraryItem
 import com.OxGames.Pluvia.ui.enums.AppFilter
 import java.util.EnumSet
 
-// TODO:
-//  1. Missing games?? Did I break something or...?
-//  2. The filter chips do not change their selected color.
-//  3. Close button on sheet or not? Tapping outside or swipe down dismisses it.
-
 data class LibraryState(
     val appInfoSortType: EnumSet<AppFilter> = PrefManager.libraryFilter,
     val appInfoList: List<LibraryItem> = emptyList(),
