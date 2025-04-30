61aeb9fa88951395bfbef725dd9f6b81e387cb31
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2025-04-30 20:34:59 +0530

    First attempt at integrating changes for glibc, assets have been gitignored.

--- app/src/main/java/com/winlator/container/Container.java
+++ app/src/main/java/com/winlator/container/Container.java
@@ -54,6 +54,7 @@ public class Container {
     private String box64Preset = Box86_64Preset.COMPATIBILITY;
     private File rootDir;
     private JSONObject extraData;
+    private int rcfileId = 0;
 
     private String lc_all = "";
 
@@ -276,6 +277,14 @@ public class Container {
         this.desktopTheme = desktopTheme;
     }
 
+    public int getRCFileId() {
+        return rcfileId;
+    }
+
+    public void setRcfileId(int id) {
+        rcfileId = id;
+    }
+
     public Iterable<String[]> drivesIterator() {
         return drivesIterator(drives);
     }
@@ -332,6 +341,7 @@ public class Container {
             data.put("box64Preset", box64Preset);
             data.put("desktopTheme", desktopTheme);
             data.put("extraData", extraData);
+            data.put("rcfileId", rcfileId);
             data.put("lc_all", lc_all);
 
             if (!WineInfo.isMainWineVersion(wineVersion)) data.put("wineVersion", wineVersion);
@@ -409,6 +419,9 @@ public class Container {
                     break;
                 case "desktopTheme" :
                     setDesktopTheme(data.getString(key));
+                    break;
+                case "rcfileId" :
+                    setRcfileId(data.getInt(key));
                     break;
                 case "lc_all" :
                     setLC_ALL(data.getString(key));
